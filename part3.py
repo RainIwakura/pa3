afe0381342401ca9675ee157f295d3a2716a84ad
Rednecked_Crake <almas.abdibayev.gr@dartmouth.edu>
2019-03-03 01:16:29 -0500

    Update part3.py

--- part3.py
+++ part3.py
@@ -19,6 +19,45 @@ from nav_msgs.msg import Odometry
 class DiffDrive():
     def __init__(self):
         self.goal_state = (rospy.get_param('/wamv/desired_point/x'), rospy.get_param('/wamv/desired_point/x'))
+        rospy.init_node("wamv_diff")
+
+
+        self.current_coord = None
+        self.cmd_drive_data = None
+        self.measurement_data = None
+        rospy.Subscriber("/cmd_drive", UsvDrive, self.cmd_drive_callback)
+        rospy.Subscriber("/wamv/odom", Odometry, self.diff_drive_callback)
+
+
+        self.publisher = rospy.Publisher("/cmd_drive", UsvDrive, queue_size=10)
+
+        rospy.spin()
+
+
+
+    def measurement_callback(self, measurement):
+        self.measurement_data = (measurement.longitude, measurement.latitude)
         
+
+    def get_xyz(self):
+        xy = self.current_coord.pose.pose.position
+        angle_with_respect_to_axis =  self.current_coord.pose.pose.orientation
+
+        return (xy.x, xy.y, angle_with_respect_to_axis.z)
+
+
+    def cmd_drive_callback(self, data):
+        self.cmd_drive_data = data
+
+
+    def diff_drive_callback(self):
+
+
+
+
+
+
+
+
 
 
